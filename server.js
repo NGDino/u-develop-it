48f4f49b941e75c60b45cc1498572f739d310d7c
D-Know <n.dino90@gmail.com>
2020-08-19 11:58:36 -0600

    routes to look up parties, delete parites and change parties

--- server.js
+++ server.js
@@ -68,33 +68,41 @@ app.get('/api/candidate/:id', (req, res) => {
 app.delete('/api/candidate/:id', (req, res) => {
     const sql = `DELETE FROM candidates WHERE id = ?`;
     const params = [req.params.id];
-    db.run(sql, params, function(err, result) {
-      if (err) {
-        res.status(400).json({ error: res.message });
-        return;
-      }
-  
-      res.json({
-        message: 'successfully deleted',
-        changes: this.changes
-      });
+    db.run(sql, params, function (err, result) {
+        if (err) {
+            res.status(400).json({
+                error: res.message
+            });
+            return;
+        }
+
+        res.json({
+            message: 'successfully deleted',
+            changes: this.changes
+        });
     });
-  });
+});
 
 // create a candidate
-app.post('/api/candidate', ({ body}, res) =>{
+app.post('/api/candidate', ({
+    body
+}, res) => {
     const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
-    if(errors){
-        res.status(400).json({ error: errors });
+    if (errors) {
+        res.status(400).json({
+            error: errors
+        });
         return;
     }
     const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
                 VALUES (?,?,?)`;
     const params = [body.first_name, body.last_name, body.industry_connected];
     //ES5 function not arrow function to use 'this'
-    db.run(sql, params, function(err, result){
-        if (err){
-            res.status(400).json({ error: err.message });
+    db.run(sql, params, function (err, result) {
+        if (err) {
+            res.status(400).json({
+                error: err.message
+            });
             return;
         }
 
@@ -104,8 +112,96 @@ app.post('/api/candidate', ({ body}, res) =>{
             id: this.lastID
         });
     })
-    
-}); 
+
+});
+
+//get all parties
+app.get('/api/parites', (req, res) => {
+    const sql = `SELECT * FROM parties`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(500).json({
+                error: err.message
+            });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+//get single party
+app.get('/api/party/:id', (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({
+                error: err.message
+            });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+
+//delete party
+app.delete('/api/party/:id', (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function (err, result) {
+        if (err) {
+            res.status(400).json({
+                error: err.message
+            });
+            return;
+        }
+        res.json({
+            message: 'successfully deleted',
+            changes: this.changes
+        });
+    });
+});
+
+
+//update party
+app.put('/api/candidate/:id', (req, res) => {
+    const errors = inputCheck(req.body, 'party_id');
+
+    if (errors) {
+        res.status(400).json({
+            error: errors
+        });
+        return;
+    }
+    const sql = `UPDATE candidates SET party_id = ?
+                 WHERE id = ?`;
+    const params = [req.body.party_id, req.params.id];
+
+    db.run(sql, params, function (err, result) {
+        if (err) {
+            res.status(400).json({
+                error: err.message
+            });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            dada: req.body,
+            changes: this.changes
+        });
+    });
+});
 
 // Default response for any other request(Not Found) Catch all
 app.use((req, res) => {
