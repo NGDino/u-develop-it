9c4ea13593aa460aea056004c47676b74816a614
D-Know <n.dino90@gmail.com>
2020-08-20 16:44:34 -0600

    WORKING thanks to bcs help!

--- server.js
+++ server.js
@@ -3,28 +3,60 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 // const sqlite3 = require('sqlite3').verbose(); // to database.js
 const inputCheck = require('./utils/inputCheck');
+const bodyParser = require('body-parser')
 
 //link to database.js
 const db = require('./db/database');
 
 //connect to api routes
 const apiRoutes = require('./routes/apiRoutes');
-app.use('/api', apiRoutes);
 
-app.use(express.urlencoded({
-    extended: true
+
+app.use(bodyParser.urlencoded({
+    extended: false
 }));
-app.use(express.json());
-
+app.use(bodyParser.json());
+app.use('/api', apiRoutes);
 // Default response for any other request(Not Found) Catch all
 app.use((req, res) => {
-    res.status(404).end();
+    res.status(404).end();
 });
 
 //start server after db connection
 db.on('open', () => {
-    app.listen(PORT, () => {
-        console.log(`server running on ${PORT}`)
+    app.listen(PORT, () => {
+        console.log(`server running on ${PORT}`)
 
-    });
+    });
 });
+
+// const express = require('express');
+// const PORT = process.env.PORT || 3001;
+// const app = express();
+// // const sqlite3 = require('sqlite3').verbose(); // to database.js
+// const inputCheck = require('./utils/inputCheck');
+
+// //link to database.js
+// const db = require('./db/database');
+
+// //connect to api routes
+// const apiRoutes = require('./routes/apiRoutes');
+// app.use('/api', apiRoutes);
+
+// app.use(express.urlencoded({
+//     extended: true
+// }));
+// app.use(express.json());
+
+// // Default response for any other request(Not Found) Catch all
+// app.use((req, res) => {
+//     res.status(404).end();
+// });
+
+// //start server after db connection
+// db.on('open', () => {
+//     app.listen(PORT, () => {
+//         console.log(`server running on ${PORT}`)
+
+//     });
+// });
